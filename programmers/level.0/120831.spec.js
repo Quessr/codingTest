e2065c3aa1b271b2e8643b74ca71aa66ff79d1df
seraheo <heosera76@gmail.com>
2022-10-20 21:17:35 +0900

    짝수의 합

---
+++ programmers/level.0/120831.spec.js
@@ -0,0 +1,28 @@
+// 문제: 짝수의 합
+const assert = require("assert");
+
+function solution(n) {
+  // let answer = 0;
+  // for (let i = 0; i <= n; i++) {
+  //   if (i % 2 === 0) {
+  //     answer = answer + i;
+  //   }
+  // }
+  // return answer;
+
+  let answer = 0;
+  for (let i = 2; i <= n; i + 2) {
+    answer = answer + 2;
+  }
+  return answer;
+}
+
+describe("짝수의 합", () => {
+  it("case 1", () => {
+    assert.equal(solution(10), 30);
+  });
+
+  it("case 2", () => {
+    assert.equal(solution(4), 6);
+  });
+});
