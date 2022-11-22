a034da1a1e11339201e935e84eb01b09a6cb7ae3
seraheo <heosera76@gmail.com>
2022-11-22 23:18:09 +0900

    평행 답안 수정

--- programmers/level.0/120875.spec.js
+++ programmers/level.0/120875.spec.js
@@ -1,21 +1,35 @@
-// 문제: 평행 
+// 문제: 평행
 const assert = require("assert");
 
 function solution(dots) {
-  var x = dots.map(x => x[0]).sort((a, b) => a - b);
-  const y = dots.map(y => y[1]).sort((a, b) => a - b)
-if ((y[1] - y[0]) / (x[1] - x[0]) === (y[3] - y[2]) / (x[3] - x[2])) {
-return 1
-}
-return 0
+  const D = dots.sort((a, b) => a[0] - b[0]);
+  const l1 = (D[1][1] - D[0][1]) / (D[1][0] - D[0][0]);
+  const l2 = (D[3][1] - D[2][1]) / (D[3][0] - D[2][0]);
+  return Number(l1 === l2);
 }
 
 describe("평행", () => {
   it("case 1", () => {
-    assert.deepEqual(solution([[1, 4], [9, 2], [3, 8], [11, 6]]), 1);
+    assert.deepEqual(
+      solution([
+        [1, 4],
+        [9, 2],
+        [3, 8],
+        [11, 6],
+      ]),
+      1
+    );
   });
 
   it("case 2", () => {
-    assert.deepEqual(solution([[3, 5], [4, 1], [2, 4], [5, 10]]), 0);
+    assert.deepEqual(
+      solution([
+        [3, 5],
+        [4, 1],
+        [2, 4],
+        [5, 10],
+      ]),
+      0
+    );
   });
 });
